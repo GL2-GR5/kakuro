4f8cf0ef40143e937c6b7725bbb6f2dea8d8b3d5
Luca <100572731+IzeLeam@users.noreply.github.com>
2024-01-29 11:14:16 +0100

    init javafx (#8)

--- src/main/java/fr/mcgcorp/Main.java
+++ src/main/java/fr/mcgcorp/Main.java
@@ -1,8 +1,24 @@
 package fr.mcgcorp;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    launch();
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("app.fxml"));
+    Parent root = loader.load();
+    stage.setScene(new Scene(root));
+    stage.setResizable(false);
+    stage.show();
   }
 }
