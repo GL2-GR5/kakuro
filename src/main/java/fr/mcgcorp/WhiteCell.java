e878665374d507edf27d7159ca22200574ea1fb4
meo <83820503+meo-pill@users.noreply.github.com>
2024-02-06 00:43:45 +0100

    doc: ajout des commentaire du code

--- src/main/java/fr/mcgcorp/WhiteCell.java
+++ src/main/java/fr/mcgcorp/WhiteCell.java
@@ -8,11 +8,20 @@ import java.util.HashMap;
  * @see Cell
  */
 class WhiteCell extends Cell {
+
+  /** Valeur de résultat de la case. */
   private int correctValue;
+  /** Valeur entre par le joueur. */
   private int value;
 
+  /** Liste des notes de la case. */
   private HashMap<Integer, Boolean> notes = new HashMap<Integer, Boolean>();
 
+  /**
+   * Constructeur d'une cellule blanche.
+   *
+   * @param correctValue La valeur correcte de la cellule.
+   */
   public WhiteCell(int correctValue) {
     this.correctValue = correctValue;
     this.value = 0;
@@ -20,38 +29,46 @@ class WhiteCell extends Cell {
       notes.put(i, false);
     }
   }
-
+  
+  /**
+   * Renvoie la valeur correcte de la case. 
+   *
+   * @return int 
+   */
   public int getCorrectValue() {
     return this.correctValue;
   }
 
-  public void setCorrectValue(int correctValue) {
-    this.correctValue = correctValue;
-  }
-
+  /**
+   * Renvoie la valeur entrée par le joueur.
+   * 
+   * @return int
+   */
   public int getValue() {
     return this.value;
   }
 
-  @Override
-  public boolean setValue(int value) {
-    this.value = value;
-    return true;
-  }
-
-  @Override
-  public boolean isModifiable() {
-    return true;
-  }
-
+  /**
+   * Ajoute une valeur en note à la case.
+   *
+   * @param valeurNote note à ajouter
+   */
   public void addNote(int valeurNote) {
     this.notes.put(valeurNote, true);
   }
 
+  /**
+   * Retire une valeur des notes de la case.
+   *
+   * @param valeurNote note à retirer
+   */
   public void removeNote(int valeurNote) {
     this.notes.put(valeurNote, false);
   }
 
+  /**
+   * Efface la valeur entrée par le joueur.
+   */
   public void clearCell() {
     this.value = 0;
     for (int i : notes.keySet()) {
@@ -59,9 +76,20 @@ class WhiteCell extends Cell {
     }
   }
 
+  /**
+   * Efface les notes de la case.
+   */
   public void clearNotes() {
     for (int i : notes.keySet()) {
       notes.put(i, false);
     }
+  }
+
+  /**
+   * Efface la valeur entrée par le joueur et les notes de la case.
+   */
+  public void clearAll() {
+    this.clearCell();
+    this.clearNotes();
   }
 }
