f0188fa730d24092c31dc46b246405202863019f
meo <83820503+meo-pill@users.noreply.github.com>
2024-02-07 09:58:04 +0100

    fix: compile CellError, MoveNote

--- src/main/java/fr/mcgcorp/CellError.java
+++ src/main/java/fr/mcgcorp/CellError.java
@@ -1,40 +1,41 @@
 package fr.mcgcorp;
 
-/** Détail l'erreur de saisie sur une cellule.
+/**
+ * Détail l'erreur de saisie sur une cellule.
+ *
  * @author PECHON Erwan
  */
 class CellError {
-	/** Coordonée de la case où l'erreur à eu lieu. */
-	protected Coord coord;
-	/** L'erreur détecté sur cette cellule. */
-	protected TypeError error;
+  /** Coordonnée de la case où l'erreur à eu lieu. */
+  protected Coord coord;
+  /** L'erreur détecté sur cette cellule. */
+  protected TypeError error;
 
-	/** Constructeur de rapport d'erreur
-	 * @author PECHON Erwan
-	 * @param coord Les coordonnées de la cellule où une erreur à était déteté.
-	 * @param error Le type d'erreur détecté.
-	 */
-	CellError(Coord coord, TypeError error){
-		this.coord = coord;
-		this.error = error;
-	}
+  /**
+   * Constructeur de rapport d'erreur
+   *
+   * @param coord Les coordonnées de la cellule où une erreur à était déteté.
+   * @param error Le type d'erreur détecté.
+   */
+  CellError(Coord coord, TypeError error) {
+    this.coord = coord;
+    this.error = error;
+  }
 
-	/** Accesseur sur les coordonnée de la case où l'erreur à eu lieu.
-	 * @author PECHON Erwan
-	 *
-	 * Permet au jeu de connaître la case associé à cette erreur.
-	 *
-	 */
-	public Coord getCoord(){
-		return this.cell;
-	}
-	/** Accesseur sur le type d'erreur détécté.
-	 * @author PECHON Erwan
-	 *
-	 * Permet au jeu de connaître le type d'erreur ayant eu lieu.
-	 *
-	 */
-	public TypeError getTypeError(){
-		return this.error;
-	}
+  /**
+   * Accesseur sur les coordonnée de la case où l'erreur à eu lieu.
+   * Permet au jeu de connaître la case associé à cette erreur.
+   */
+  public Coord getCoord() {
+    return this.coord;
+  }
+
+  /**
+   * Accesseur sur le type d'erreur détecté.
+   * Permet au jeu de connaître le type d'erreur ayant eu lieu.
+   * 
+   */
+  public TypeError getTypeError() {
+    return this.error;
+  }
 }
--- src/main/java/fr/mcgcorp/MoveNote.java
+++ src/main/java/fr/mcgcorp/MoveNote.java
@@ -1,43 +1,46 @@
 package fr.mcgcorp;
 
-import java.util.HashSet
+import java.util.HashSet;
 
 /**
  * Représentation d'un mouvement modifiant les notes d'une case.
+ *
  * @see Move
  * @author PECHON Erwan
  */
 class MoveNote extends Move {
-	/** Notes modifié par le joueur. */
-	protected HashSet<Integer> notes_old;
-	/** Nouvelle notes saisie par le joueur. */
-	protected HashSet<Integer> notes_new;
+  /** Notes modifié par le joueur. */
+  protected HashSet<Integer> notes_old;
+  /** Nouvelle notes saisie par le joueur. */
+  protected HashSet<Integer> notes_new;
 
-	/**
-	 * Constructeur d'un mouvement modifiant les notes.
-	 *
-	 * @param notes_old Les notes de la cellule, avant ce mouvement.
-	 * @param notes_new Les notes de la cellule, après ce mouvement.
-	 */
-	MoveValue(HashSet<Integer> notes_old, HashSet<Integer> notes_new){
-		super();
-		this.notes_old = notes_old;
-		this.notes_new = notes_new;
-	}
+  /**
+   * Constructeur d'un mouvement modifiant les notes.
+   *
+   * @param notes_old Les notes de la cellule, avant ce mouvement.
+   * @param notes_new Les notes de la cellule, après ce mouvement.
+   */
+  MoveNote(HashSet<Integer> notes_old, HashSet<Integer> notes_new) {
+    super();
+    this.notes_old = notes_old;
+    this.notes_new = notes_new;
+  }
 
-	/**
-	 * Renvoie les anciennes notes de la case.
-	 * @return Le vieux set de notes
-	 */
-	public HashSet<Integer> getNotes_old(){
-		return this.notes_old;
-	}
-	/**
-	 * Renvoie les nouvelles notes de la case.
-	 * @return Le nouveaux set de notes
-	 */
-	public HashSet<Integer> getNotes_new(){
-		return this.notes_new;
-	}
+  /**
+   * Renvoie les anciennes notes de la case.
+   *
+   * @return Le vieux set de notes
+   */
+  public HashSet<Integer> getNotes_old() {
+    return this.notes_old;
+  }
+
+  /**
+   * Renvoie les nouvelles notes de la case.
+   *
+   * @return Le nouveaux set de notes
+   */
+  public HashSet<Integer> getNotes_new() {
+    return this.notes_new;
+  }
 }
-
