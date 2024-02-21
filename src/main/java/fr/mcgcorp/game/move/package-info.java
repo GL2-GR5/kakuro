f32f219e68f03316fef140ac5d54d2fc07dc33df
meo <83820503+meo-pill@users.noreply.github.com>
2024-02-21 16:20:42 +0100

    add: ajout des diagrame dans la doc

--- src/main/java/fr/mcgcorp/game/move/package-info.java
+++ src/main/java/fr/mcgcorp/game/move/package-info.java
@@ -1,1 +1,6 @@
+/**
+ * Package gérant les déplacements des entités.
+ * <br>
+ * <img src="doc-files/classes_move.svg" alt="Diagramme de classes du package fr.mcgcorp" width="100%" />
+ */
 package fr.mcgcorp.game.move;
--- src/main/java/fr/mcgcorp/package-info.java
+++ src/main/java/fr/mcgcorp/package-info.java
@@ -2,6 +2,8 @@
  * Ce package est le package racine du jeu.
  * <br>
  * Il contient les classes principales du jeu.
+ * <br>
+ * <img src="doc-files/classes.svg" alt="Diagramme de classes du package fr.mcgcorp" width="100%" />
  * 
  * @author GOUDET Lucas, HOUGET Julien, LAHMAR Marwan, LE LUET Hôa, PECHON Erwan, POURCEAU Luca, PUREN Mewen 
  * @version 0.1
