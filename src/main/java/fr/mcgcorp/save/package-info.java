712a0af05ac30836ad4c252c844851ca8c02ac11
ABTonniere <91685633+ABTonniere@users.noreply.github.com>
2024-02-23 10:15:40 +0100

    add: Ajout de packages pour les différents types sauvegardes

---
+++ src/main/java/fr/mcgcorp/save/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Package responsable de la sauvegarde des données du jeu.
+ * <br>
+ * Ce package contient des sous-packages pour différents éléments de sauvegarde,
+ * notamment les profils, les classements et les sauvegardes de parties.
+ * <br>
+ *
+ * @author HOUGET Julien, LE LUET Hôa
+ * @version 0.1
+ * @since 23-02-2024
+ */
+package fr.mcgcorp.save;
---
+++ src/main/java/fr/mcgcorp/save/profil/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Package responsable des profils des joueurs.
+ * <br>
+ * Ce package contient les informations des joueurs comme leur nom, leurs sauvegardes de parties.
+ * <br>
+ *
+ * @author LE LUET Hôa
+ * @version 0.1
+ * @since 23-02-2024
+ */
+package fr.mcgcorp.save.profil;
