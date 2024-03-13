5472cd61fa3cf68d1b5743c7fb9f4b63b0116b9c
Luca <luca.pourceau@gmail.com>
2024-03-13 22:51:01 +0100

    fix : window position

--- src/main/java/fr/mcgcorp/fxml/controllers/OverController.java
+++ src/main/java/fr/mcgcorp/fxml/controllers/OverController.java
@@ -2,14 +2,14 @@ package fr.mcgcorp.fxml.controllers;
 
 import fr.mcgcorp.fxml.managers.ControllerManager;
 import javafx.scene.effect.BoxBlur;
+import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javax.naming.ldap.Control;
 
 public abstract class OverController extends Controller {
 
-  private Controller parent;
+  private boolean blurParent;
 
   abstract String getPathToFxml();
 
@@ -23,15 +23,27 @@ public abstract class OverController extends Controller {
     return this.stage;
   }
 
+  public final void close() {
+    if (this.blurParent) {
+      ControllerManager.getInstance().getCurrentController().getStage().getScene().getRoot().setEffect(null);
+    }
+    this.getStage().close();
+  }
+
   @Override
   public final void show() {
-    this.parent = ControllerManager.getInstance().getCurrentController();
-    //this.parent.getStage().getScene().getRoot().setEffect(new BoxBlur());
-    this.stage.setOnCloseRequest(event -> {
-      System.out.println("Closing");
-      this.parent.getStage().getScene().getRoot().setEffect(null);
-      this.parent = null;
-    });
+    Stage parent = ControllerManager.getInstance().getCurrentController().getStage();
+
+    final double stageWidth = ((Pane) this.getStage().getScene().getRoot()).getPrefWidth();
+    final double stageHeight = ((Pane) this.getStage().getScene().getRoot()).getPrefHeight();
+    final double x = parent.getX() + parent.getWidth() / 2 - stageWidth / 2;
+    final double y = parent.getY() + parent.getHeight() / 2 - stageHeight / 2;
+    this.getStage().setX(x);
+    this.getStage().setY(y);
+
+    if (this.blurParent) {
+      parent.getScene().getRoot().setEffect(new BoxBlur(3, 3, 3));
+    }
     this.stage.show();
   }
 }
