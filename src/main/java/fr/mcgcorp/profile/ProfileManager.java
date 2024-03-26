23d80111de88cc8c879d475c3772fc1d36e7a855
meo <83820503+meo-pill@users.noreply.github.com>
2024-03-26 13:35:35 +0100

    merge: implemntation des profile (#62)
    * add: implemntation des profile
    
    Co-Authored-By: Julien <79098180+smalltheif@users.noreply.github.com>
    
    * update: profile management
    
    * fix(doc): documentation de ProfileManager
    
    Co-Authored-By: Luca <100572731+IzeLeam@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Julien <79098180+smalltheif@users.noreply.github.com>
    Co-authored-by: Luca <luca.pourceau@gmail.com>
    Co-authored-by: Luca <100572731+IzeLeam@users.noreply.github.com>

---
+++ src/main/java/fr/mcgcorp/profile/ProfileManager.java
@@ -0,0 +1,91 @@
+package fr.mcgcorp.profile;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Classe permettant de gérer les profils.
+ * 
+ * @author POURCEAU Luca
+ */
+public class ProfileManager {
+  /** Instance du Profile manager. */
+  private static final ProfileManager instance = new ProfileManager();
+  /** La liste des profile. */
+  private final Set<Profile> profiles = new HashSet<>();
+
+  /**
+   * permet de récupérer l'instance de la classe.
+   * 
+   * @return l'instance de la classe
+   */
+  public static ProfileManager getInstance() {
+    return instance;
+  }
+
+  /**
+   * permet d'ajouter un profil.
+   * 
+   * @param profile le profil à ajouter
+   */
+  public void addProfile(Profile profile) {
+    profiles.add(profile);
+  }
+
+  /**
+   * permet de supprimer un profil.
+   * 
+   * @param profile le profil à supprimer
+   */
+  public void removeProfile(Profile profile) {
+    profiles.remove(profile);
+  }
+
+  /**
+   * permet de récupérer la liste des profils.
+   * 
+   * @return la liste des profils
+   */
+  public Set<Profile> getProfiles() {
+    return profiles;
+  }
+
+  /**
+   * permet de récupérer un profil par son nom.
+   * 
+   * @param name le nom du profil
+   * @return le profil
+   */
+  public Profile getProfile(String name) {
+    for (Profile profile : profiles) {
+      if (profile.getName().equals(name)) {
+        return profile;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * permet de savoir si la liste des profils est vide.
+   * 
+   * @return true si la liste est vide, false sinon
+   */
+  public boolean isEmpty() {
+    return profiles.isEmpty();
+  }
+
+  /**
+   * permet de savoir si un profil existe.
+   * 
+   * @param name le nom du profil
+   * @return true si le profil existe, false sinon
+   */
+  public boolean exists(String name) {
+    for (Profile profile : profiles) {
+      if (profile.getName().equals(name)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
