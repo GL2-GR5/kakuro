09afd8e41e5cfc4f38925fa56d6201eb460d9946
meo <83820503+meo-pill@users.noreply.github.com>
2024-03-28 15:51:36 +0100

    doc: ajout de comentaire sur GridManager

--- src/main/java/fr/mcgcorp/managers/GridManager.java
+++ src/main/java/fr/mcgcorp/managers/GridManager.java
@@ -3,16 +3,39 @@ package fr.mcgcorp.managers;
 import fr.mcgcorp.managers.JsonFile;
 import java.io.IOException;
 
+/**
+ * Cette classe est utilisée pour gérer les grilles de jeu.
+ *
+ * @version 1.0
+ * @since 1.0
+ */
 public class GridManager {
+  /**
+   * Le singleton de la classe GridManager.
+   */
   private static final GridManager instance = new GridManager();
+  /**
+   * Le fichier JSON contenant les grilles de jeu.
+   */
   private final JsonFile file;
 
+  /**
+   * Obtenir l'instance de GridManager.
+   *
+   * @return l'instance de GridManager.
+   */
   public static GridManager getInstance() {
     return instance;
   }
 
+  /**
+   * Constructeur de la classe GridManager.
+   */
   private GridManager() {
     JsonFile file = null;
+    /**
+     * Essayer de créer un fichier JSON contenant les grilles de jeu.
+     */
     try {
       file = new JsonFile("grids.json");
     } catch (IOException e) {
@@ -22,19 +45,44 @@ public class GridManager {
     this.file = file;
   }
 
+  /**
+   * Obtenir une grille de jeu par son identifiant.
+   *
+   * @param id l'identifiant de la grille de jeu.
+   * @return la grille de jeu.
+   */
   public String getGridById(int id) {
     return file.getString("grids." + id + ".serialized");
   }
 
+  /**
+   * Obtenir une grille de jeu par sa difficulté.
+   *
+   * @param difficulty la difficulté de la grille de jeu.
+   * @return la grille de jeu.
+   */
   public enum GridDifficulty {
     EASY("easy"), MEDIUM("medium"), HARD("hard");
 
+    /**
+     * La clé de la difficulté.
+     */
     private final String key;
 
+    /**
+     * Constructeur de la classe GridDifficulty.
+     *
+     * @param key la clé de la difficulté.
+     */
     GridDifficulty(String key) {
       this.key = key;
     }
 
+    /**
+     * Obtenir la clé de la difficulté.
+     *
+     * @return la clé de la difficulté.
+     */
     private String getKey() {
       return key;
     }
